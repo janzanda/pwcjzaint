a82863af4b4a6e8fd1b4b0e8f390b87f03ce3058
Jan Zanda <jan.zanda@i.cz>
2021-11-05 14:19:40 +0100

    Artifact name Javadoc

--- src/main/java/com/pwc/interview/janzanda/RouteFinderApplication.java
+++ src/main/java/com/pwc/interview/janzanda/RouteFinderApplication.java
@@ -3,11 +3,14 @@ package com.pwc.interview.janzanda;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring boot app. Generated.
+ */
 @SpringBootApplication
 public class RouteFinderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RouteFinderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RouteFinderApplication.class, args);
+    }
 
 }
